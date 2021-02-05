db5fcd3f1fcd7231333f7ac3301bc5dda9d0ad54
Pinky <janeletageng28@gmail.com>
2021-02-05 10:35:29 +0200

    add address to allowed hosts

--- dentist/settings.py
+++ dentist/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o+2p8ctie=adeomar3$b+l=-)x2#wufaruka8+#j&=)p$5odaud#f4*phb!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mazinyo-dental.herokuapp.com']
 
 
 # Application definition
