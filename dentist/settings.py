11d3c462885b428b54d1d7b513b1f87f144609e8
Pinky <janeletageng28@gmail.com>
2021-02-05 10:23:14 +0200

    fix procfile

--- dentist/settings.py
+++ dentist/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',       # to host heroku
+    'whitenoise.middleware.WhiteNoiseMiddleware',       # to host heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
